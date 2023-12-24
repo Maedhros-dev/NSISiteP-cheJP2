9220f1f0fb52d1769492da366cbe286fe665ef1f
Maedhros-dev <70408526+Maedhros-dev@users.noreply.github.com>
2023-12-24 18:34:06 +0000

    test de form

---
+++ action.php
@@ -0,0 +1,29 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Récupérer les données du formulaire
+    $nom = $_POST["nom"];
+    $email = $_POST["email"];
+    $message = $_POST["message"];
+
+    // Adresse e-mail où vous souhaitez recevoir le message
+    $destinataire = "votre@email.com";
+
+    // Sujet du message
+    $sujet = "Nouveau message de $nom";
+
+    // Construire le corps du message
+    $corps_message = "Nom: $nom\n";
+    $corps_message .= "Email: $email\n";
+    $corps_message .= "Message:\n$message";
+
+    // En-têtes du message
+    $entetes = "De: $email\r\n";
+
+    // Envoyer l'e-mail
+    if (mail($destinataire, $sujet, $corps_message, $entetes)) {
+        echo "Votre message a été envoyé avec succès.";
+    } else {
+        echo "Erreur lors de l'envoi du message.";
+    }
+}
+?>
