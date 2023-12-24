ccd91d85b4a03171738018b82ca0683822fbfc2c
Maedhros-dev <70408526+Maedhros-dev@users.noreply.github.com>
2023-12-24 23:11:43 +0000

    afed

--- action.php
+++ action.php
@@ -1,4 +1,34 @@
 <?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require $_SERVER['DOCUMENT_ROOT'] . '/mail/Exception.php';
+require $_SERVER['DOCUMENT_ROOT'] . '/mail/PHPMailer.php';
+require $_SERVER['DOCUMENT_ROOT'] . '/mail/SMTP.php';
+
+$mail = new PHPMailer;
+$mail->isSMTP(); 
+$mail->SMTPDebug = 2; // 0 = off (for production use) - 1 = client messages - 2 = client and server messages
+$mail->Host = "smtp.gmail.com"; // use $mail->Host = gethostbyname('smtp.gmail.com'); // if your network does not support SMTP over IPv6
+$mail->Port = 587; // TLS only
+$mail->SMTPSecure = 'tls'; // ssl is deprecated
+$mail->SMTPAuth = true;
+$mail->Username = 'iopnyuil@gmail.com'; // email
+$mail->Password = 'WJEh1G*(#G6<)e8c'; // password
+$mail->setFrom('system@cksoftwares.com', 'CKSoftwares System'); // From email and name
+$mail->addAddress('elijah.tallur@proton.me', 'Mr. Moi'); // to email and name
+$mail->Subject = 'PHPMailer GMail SMTP test';
+$mail->msgHTML("test body"); //$mail->msgHTML(file_get_contents('contents.html'), __DIR__); //Read an HTML message body from an external file, convert referenced images to embedded,
+$mail->AltBody = 'HTML messaging not supported'; // If html emails is not supported by the receiver, show this body
+// $mail->addAttachment('images/phpmailer_mini.png'); //Attach an image file
+$mail->SMTPOptions = array(
+                    'ssl' => array(
+                        'verify_peer' => false,
+                        'verify_peer_name' => false,
+                        'allow_self_signed' => true
+                    )
+                );
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Récupérer les données du formulaire
     $nom = $_POST["nom"];
@@ -6,7 +36,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $message = $_POST["message"];
 
     // Adresse e-mail où vous souhaitez recevoir le message
-    $destinataire = "elijah.tallur@proton.me";
+    $destinataire = "test";
 
     // Sujet du message
     $sujet = "Nouveau message de $nom";
@@ -17,13 +47,13 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $corps_message .= "Message:\n$message";
 
     // En-têtes du message
-    $entetes = "De: $email\r\n";
+    $entetes = "De: $destinataire\r\n";
 
     // Envoyer l'e-mail
-    if (mail($destinataire, $sujet, $corps_message, $entetes)) {
-        echo "Votre message a été envoyé avec succès.";
-    } else {
-        echo "Erreur lors de l'envoi du message.";
+    if(!$mail->send()){
+        echo "Mailer Error: " . $mail->ErrorInfo;
+    }else{
+        echo "Message sent!";
     }
 }
 ?>
